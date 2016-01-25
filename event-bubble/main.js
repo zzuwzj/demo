a3d1dc6dc63b52ec981c1860aeeda3b2e83739a4
Zhenjun Wang <zzuwzj@163.com>
2016-01-25 15:20:39 +0800

    Update demo event bubble
    Add event listener for capture phase

--- event-bubble/main.js
+++ event-bubble/main.js
@@ -3,13 +3,28 @@ c1.addEventListener(
   'click',
   function (evt) {
     if (evt.target.className == "box c4") {
-      alert('c4');
+      alert('bubble c4');
     } else if (evt.target.className == "box c3") {
-      alert('c3');
+      alert('bubble c3');
     } else if (evt.target.className == "box c2") {
-      alert('c2');
+      alert('bubble c2');
     } else if (evt.target.className == "box c1") {
-      alert('c1');
+      alert('bubble c1');
     }
     console.log(evt);
   });
+
+c1.addEventListener(
+  'click',
+  function (evt) {
+    if (evt.target.className == "box c4") {
+      alert('capture c4');
+    } else if (evt.target.className == "box c3") {
+      alert('capture c3');
+    } else if (evt.target.className == "box c2") {
+      alert('capture c2');
+    } else if (evt.target.className == "box c1") {
+      alert('capture c1');
+    }
+    console.log(evt);
+  },true);
