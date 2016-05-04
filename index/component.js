62e7de4be89c474a72cb9f2e0da9fd50698f9820
Zhenjun <zzuwzj@163.com>
2016-05-04 14:12:28 +0800

    fix

--- index/component.js
+++ index/component.js
@@ -9,6 +9,7 @@ var DemoWall = React.createClass({
             );
         });
         return (
+            <h1>Demos</h1>
             <ul>
                 {demos}
             </ul>
--- index/index.js
+++ index/index.js
@@ -5,8 +5,9 @@
     })
     .done(function(data) {
         console.log(data);
+        var demoData = JSON.parse(data.responseText);
         ReactDOM.render(
-          <DemoWall products={data} />,
+          <DemoWall products={demoData} />,
           document.getElementById('container')
         );
     })
