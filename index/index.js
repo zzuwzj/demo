3b1b5398e0e51ff3088588e5ded8249482a873b1
Zhenjun <zzuwzj@163.com>
2016-05-04 14:28:01 +0800

    fix

--- index/index.js
+++ index/index.js
@@ -6,6 +6,7 @@
     .done(function(data) {
         console.log(data);
         var demoData = JSON.parse(data.responseText);
+        console.log(demoData);
         ReactDOM.render(
           <DemoWall demos={demoData.demos} />,
           document.getElementById('container')
