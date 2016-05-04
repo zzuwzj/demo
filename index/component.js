d4a44860a22a63f58da9c4f799e5eabfb2e1337b
Zhenjun <zzuwzj@163.com>
2016-05-04 13:48:41 +0800

    refactor - use reacts in index page

---
+++ index/component.js
@@ -0,0 +1,34 @@
+/**
+ * index react component react js file
+ */
+var DemoWall = React.createClass({
+    render: function() {
+        var demos = this.props.data.map(function(demo){
+            return (
+                <DemoCard src={this.props.src} title={this.props.title} description={this.props.description} />
+            );
+        });
+        return {
+            <ul>
+                {demos}
+            </ul>
+        }
+    }
+});
+
+
+var DemoCard = React.createClass({
+    render: function() {
+        return {
+            <li>
+                <h3>
+                    <a class="anchor" href="this.props.src" aria-hidden="true">
+                        <span class="octicon octicon-link"></span>
+                        {this.props.title}
+                    </a>
+                </h3>
+                <p>{this.props.description}</p>
+            </li>
+        }
+    }
+});
---
+++ index/index.js
@@ -0,0 +1,17 @@
+(function(){
+    $.$.ajax({
+        url: 'data.json',
+        dataType: 'json'
+    })
+    .done(function(data) {
+        console.log(data);
+        ReactDOM.render(
+          <DemoWall products={data} />,
+          $('#container')
+        );
+    })
+    .fail(function(err) {
+        console.log(err);
+    });
+
+})();
