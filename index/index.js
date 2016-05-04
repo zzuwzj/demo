f1c0c3c319d8b3eea527e29abe8cf760cb891349
Zhenjun <zzuwzj@163.com>
2016-05-04 14:34:49 +0800

    test

--- index/index.js
+++ index/index.js
@@ -6,7 +6,7 @@
     .done(function(data) {
         console.log(data);
         // var demoData = JSON.parse(data.responseText);
-        var demoData = [
+        var demoData = {demos:[
         {
             "src": "fullpage/index.html",
             "title": "全屏滚动网站",
@@ -15,7 +15,7 @@
             "src": "bootstrap/index.html",
             "title": "Responsive网站",
             "description": "使用<a href=\"http: //getbootstrap.com/\">Bootstrap</a>开发的网站，可根据浏览器窗口大小自适应优化显示"
-        }];
+        }]};
         console.log(demoData);
         ReactDOM.render(
           <DemoWall demos={demoData.demos} />,
